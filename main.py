fe74b05fef95c95ae7e549df83f7573a3676e23a
BlueEgle <145373399+BlueEgle@users.noreply.github.com>
2024-02-13 10:59:24 -0500

    Pieces Rendered

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import pygame
 (width, height) = (575, 575)
 screen = pygame.display.set_mode((width, height))
 
+Piecewidth = 64 
+Pieceheight = 64
 
 colors=[pygame.Color('green'),pygame.Color('red')]
 for r in range(9):
@@ -10,9 +12,21 @@ for r in range(9):
       color=colors[(r+c)%2]
       pygame.draw.rect(screen, color, pygame.Rect(c*64,r*64,64,64))
 
+pieces = ["Rook", "Knight", "Boat", "Elephant", "King", "Queen", "Boat", "Knight", "Rook"]
+green_pieces = []
+red_pieces = []
+for i in pieces:
+  green_pieces.append(pygame.image.load("assets/Green_" + i + ".png"))
+
+for i in pieces: 
+  red_pieces.append(pygame.image.load("assets/Red_" + i + ".png"))
 
 
-pygame.display.set_caption('Special Chess')
+for i in range (0,9):
+    screen.blit(green_pieces[i], ((i*64),0))
+    screen.blit(red_pieces[i], ((i*64, 512)))
+    screen.blit(pygame.image.load('assets/Red_Pawn.png'), ((i*64), 448))
+    screen.blit(pygame.image.load('assets/Green_Pawn.png'), (i*64, 64))
 
 
 
@@ -23,6 +37,8 @@ pygame.display.flip()
 
 running = True
 while running:
+
   for event in pygame.event.get():
     if event.type == pygame.QUIT:
       running = False
+  pygame.display.update()
