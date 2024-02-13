6e16b65507a1cd2322fc18d9394b7310763d8edb
BlueEgle <145373399+BlueEgle@users.noreply.github.com>
2024-02-13 11:36:50 -0500

    Pieces Displayed with a 2d array

--- main.py
+++ main.py
@@ -13,22 +13,57 @@ for r in range(9):
       pygame.draw.rect(screen, color, pygame.Rect(c*64,r*64,64,64))
 
 pieces = ["Rook", "Knight", "Boat", "Elephant", "King", "Queen", "Boat", "Knight", "Rook"]
-green_pieces = []
-red_pieces = []
-for i in pieces:
-  green_pieces.append(pygame.image.load("assets/Green_" + i + ".png"))
+#green_pieces = []
+#red_pieces = []
 
-for i in pieces: 
-  red_pieces.append(pygame.image.load("assets/Red_" + i + ".png"))
+#Creating Board
+a =[]
+b =[]
+c =[]
+d = [] 
+e = []
+f = []
+g = []
+h = []
+i = []
 
+board = [a, b, c, d, e, f, g, h, i] 
+#putting pieces in board with for loop. '_' is an unoccupied space
+for i in range(0, 80):
+  if i < 9:
+    board[0].append('Green_' + pieces[i])
 
-for i in range (0,9):
-    screen.blit(green_pieces[i], ((i*64),0))
-    screen.blit(red_pieces[i], ((i*64, 512)))
-    screen.blit(pygame.image.load('assets/Red_Pawn.png'), ((i*64), 448))
-    screen.blit(pygame.image.load('assets/Green_Pawn.png'), (i*64, 64))
+  elif 8 < i < 18:
+    board[1].append('Green_Pawn')
+  elif 17 < i  < 27:
+    board[2].append('_')
+  elif 26 < i < 36:
+    board[3].append('_')
+  elif 35 < i < 45:
+    e.append('_')
+  elif 44 < i < 54:
+     f.append('_')
+  elif 53 < i < 63:
+     g.append('_')
+  elif 62 < i < 72:
+     h.append('Red_Pawn')
+  elif 71< i:
+      board[8].append('Red_' + pieces[(i-72)])
 
+board[8].append('Red_Rook')
 
+#putting board into the game
+xloc = 0
+yloc = 0
+for i in board:
+   xloc = 0
+   yloc += 64
+   for z in i:
+      if z != '_':
+        screen.blit(pygame.image.load('assets/' + z + ".png"), (xloc, (yloc - 64)))
+        xloc += 64
+
+print(board)
 
 pygame.display.flip()
 
@@ -37,6 +72,7 @@ pygame.display.flip()
 
 running = True
 while running:
+  #We're going to need a shitload of logic here 
 
   for event in pygame.event.get():
     if event.type == pygame.QUIT:
