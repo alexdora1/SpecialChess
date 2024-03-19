1bd39a28e184f9f4c64d5e165009efa3cb6a2cc2
BlueEgle <145373399+BlueEgle@users.noreply.github.com>
2024-03-19 17:10:17 -0400

    rook+elephant
    Elephants work, rooks sorta work, bug causing all pieces to be in piece class fixed

--- main.py
+++ main.py
@@ -12,16 +12,179 @@ class Piece:
         self.type = type
         self.image = image
         self.loc = loc
+    def islegal(self, ex_board, pos1,pos2):
+      print("We're in the piece")
+      return True
+
 
 class Rook(Piece):
-  def islegal(self, ex_board, pos1, pos2):
-    return True
+    def islegal(self, ex_board, pos1, pos2): 
+        
+      if self.team == 'r':       
+        #sees if the move is legal
+        if(pos1[0] < pos2[0]) and (pos2[1] == pos1[1]):
+          # finds pieces
+          i = pos1[0]
+          while i != pos2[0]: 
+            for c in ex_board:
+                if c.loc == (i, pos2[1]):
+                  return False
+            i += 64
+          for c in ex_board:
+              if c.loc == (pos2[0], pos2[1]) and c.team == 'g':
+                ex_board.pop((ex_board.index(c)))
+                return True
+              elif c.loc == (pos2[0], pos2[1]) and c.team == 'r':
+                return False
+          return True
+        elif(((pos1[0] > pos2[0]))) and ((pos2[1] == pos1[1])):
+          i = pos1[0]
+          while i != pos2[1]: 
+            for c in ex_board:
+                if c.loc == (i, pos2[1]):
+                  return False
+            i -= 64
+          for c in ex_board:
+              if c.loc == (pos2[0], pos2[1]) and c.team == 'g':
+                ex_board.pop((ex_board.index(c)))
+                return True
+              elif c.loc == (pos2[0], pos2[1]) and c.team == 'r':
+                return False
+          return True
+        
+        elif((pos1[0] == pos2[0])) and (pos1[1] > pos2[1]):
+          i = pos1[1]
+          while i != pos2[1]: 
+            for c in ex_board:
+                if c.loc == (i, pos2[1]):
+                  return False
+            i -= 64
+          for c in ex_board:
+              if c.loc == (pos2[0], pos2[1]) and c.team == 'g':
+                ex_board.pop((ex_board.index(c)))
+                return True
+              elif c.loc == (pos2[0], pos2[1]) and c.team == 'r':
+                return False
+          return True
+        elif((pos1[0] == pos2[0])) and (pos1[1] < pos2[1]):
+          i = pos1[1]
+          while i != pos2[1]: 
+            for c in ex_board:
+                if c.loc == (i, pos2[1]):
+                  return False
+            i += 64
+          for c in ex_board:
+              if c.loc == (pos2[0], pos2[1]) and c.team == 'g':
+                ex_board.pop((ex_board.index(c)))
+                return True
+              elif c.loc == (pos2[0], pos2[1]) and c.team == 'r':
+                return False
+          return True
+      else:
+        if(pos1[0] < pos2[0]) and (pos2[1] - pos1[1] == (0)):
+          # finds pieces
+          i = pos1[0]
+          while i != pos2[0]: 
+            for c in ex_board:
+                if c.loc == (i, pos2[1]):
+                  return False
+            i += 64
+          for c in ex_board:
+              if c.loc == (pos2[0], pos2[1]) and c.team == 'r':
+                ex_board.pop((ex_board.index(c)))
+                return True
+              elif c.loc == (pos2[0], pos2[1]) and c.team == 'g':
+                return False
+          return True
+        elif(((pos1[0] > pos2[0]))) and ((pos2[1] == pos1[1])):
+          i = pos1[0]
+          while i != pos2[1]: 
+            for c in ex_board:
+                if c.loc == (i, pos2[1]):
+                  return False
+            i -= 64
+          for c in ex_board:
+              if c.loc == (pos2[0], pos2[1]) and c.team == 'r':
+                ex_board.pop((ex_board.index(c)))
+                return True
+              elif c.loc == (pos2[0], pos2[1]) and c.team == 'g':
+                return False
+          return True
+        
+        elif((pos1[0] == pos2[0])) and (pos1[1] > pos2[1]):
+          i = pos1[1]
+          while i != pos2[1]: 
+            for c in ex_board:
+                if c.loc == (i, pos2[1]):
+                  return False
+            i -= 64
+          for c in ex_board:
+              if c.loc == (pos2[0], pos2[1]) and c.team == 'r':
+                ex_board.pop((ex_board.index(c)))
+                return True
+              elif c.loc == (pos2[0], pos2[1]) and c.team == 'g':
+                return False
+          return True
+        elif((pos1[0] == pos2[0])) and (pos1[1] < pos2[1]):
+          i = pos1[1]
+          while i != pos2[1]: 
+            for c in ex_board:
+                if c.loc == (i, pos2[1]):
+                  return False
+            i += 64
+          for c in ex_board:
+              if c.loc == (pos2[0], pos2[1]) and c.team == 'r':
+                ex_board.pop((ex_board.index(c)))
+                return True
+              elif c.loc == (pos2[0], pos2[1]) and c.team == 'g':
+                return False
+          return True          
+class Knight(Piece):
+    def islegal(self, ex_board, pos1, pos2):   
+      return True
+    
+class Bishop(Piece):
+    def islegal(self, ex_board, pos1, pos2):   
+      return True
+
+class King(Piece):
+    def islegal(self, ex_board, pos1, pos2):   
+      return True
+
+class Queen(Piece):
+    def islegal(self, ex_board, pos1, pos2):   
+      return True
+
+class Elephant(Piece):
+    def islegal(self, ex_board, pos1, pos2):   
+      print(abs(pos1[0] - pos2[0]))
+      print(abs(pos1[1] - pos2[1]))
+        
+      if self.team == 'r':       
+        if((abs(pos1[0] - pos2[0]) == 128)) and (abs((pos1[1] - pos2[1]) == (128))):
+          for c in ex_board:
+              if c.loc == (pos2[0], pos2[1]) and c.team == 'g':
+                ex_board.pop((ex_board.index(c)))
+                return True
+              elif c.loc == (pos2[0], pos2[1]) and c.team == 'r':
+                return False
+          return True
+
+      else:
+        if((abs(pos1[0] - pos2[0]) == 128) and (abs(pos2[1] - pos1[1]) == (128))):
+          for c in ex_board:
+              if c.loc == (pos2[0], pos2[1]) and c.team == 'r':
+                ex_board.pop((ex_board.index(c)))
+                return True
+              elif c.loc == (pos2[0], pos2[1]) and c.team == 'g':
+                return False
+          return True
+      return False  
+      
+
 
 class Pawn(Piece):
-  def islegal(self, ex_board, pos1, pos2):
-    print(pos1)
-    print(pos2)
-    print(pos2[1] - pos1[1])                  
+  def islegal(self, ex_board, pos1, pos2):              
     if self.team == 'g':
       if pos1[1] == 64:
         #this is the only part that works
@@ -34,7 +197,7 @@ class Pawn(Piece):
           return True
         elif (((pos1[0] - pos2[0] == 64) or (pos1[0] - pos2[0] == (-64))) and (pos2[1] - pos1[1]) == (64) and pos2[1] - pos1[1] > 0):
             for c in ex_board:
-              if c.loc == (pos2[0], pos2[1]):
+              if c.loc == (pos2[0], pos2[1]) and c.team == 'r':
                 ex_board.pop((ex_board.index(c)))
                 return True
     else:
@@ -49,7 +212,7 @@ class Pawn(Piece):
           return True
         elif (((pos1[0] - pos2[0] == 64) or (pos1[0] - pos2[0] == (-64))) and (pos2[1] - pos1[1]) == (-64) and pos2[1] - pos1[1] < 0):
             for c in ex_board:
-              if c.loc == (pos2[0], pos2[1]):
+              if c.loc == (pos2[0], pos2[1]) and c.team == 'g':
                 ex_board.pop((ex_board.index(c)))
                 return True
     return False
@@ -58,15 +221,15 @@ class Pawn(Piece):
 
 #Assiging places 
         
-gR1 = Piece('', '', '', (0,0))
-gN1 = Piece('', '', '', (0,0))
-gB1 = Piece('', '', '', (0,0))
-gE = Piece('', '', '', (0,0)) 
-gK = Piece('', '', '', (0,0))
-gQ = Piece('', '', '', (0,0))
-gB2= Piece('', '', '', (0,0))
-gN2 = Piece('', '', '', (0,0))
-gR2 = Piece('', '', '', (0,0))
+gR1 = Rook('', '', '', (0,0))
+gN1 = Knight('', '', '', (0,0))
+gB1 = Bishop('', '', '', (0,0))
+gE = Elephant('', '', '', (0,0)) 
+gK = King('', '', '', (0,0))
+gQ = Queen('', '', '', (0,0))
+gB2= Bishop('', '', '', (0,0))
+gN2 = Knight('', '', '', (0,0))
+gR2 = Rook('', '', '', (0,0))
 gP1 = Pawn('', '', '', (0,0))
 gP2 = Pawn('', '', '', (0,0))
 gP3 = Pawn('', '', '', (0,0))
@@ -85,15 +248,15 @@ rP6 = Pawn('', '', '', (0,0))
 rP7 = Pawn('', '', '', (0,0))
 rP8 = Pawn('', '', '', (0,0))
 rP9 = Pawn('', '', '', (0,0))
-rR1 = Piece('', '', '', (0,0))
-rB1 = Piece('', '', '', (0,0))
-rN1 = Piece('', '', '', (0,0))
-rE = Piece('', '', '', (0,0))
-rK = Piece('', '', '', (0,0))
-rQ = Piece('', '', '', (0,0))
-rB2 = Piece('', '', '', (0,0))
-rN2 = Piece('', '', '', (0,0))
-rR2 = Piece('', '', '', (0,0))
+rR1 = Rook('', '', '', (0,0))
+rB1 = Bishop('', '', '', (0,0))
+rN1 = Knight('', '', '', (0,0))
+rE = Elephant('', '', '', (0,0))
+rK = King('', '', '', (0,0))
+rQ = Queen('', '', '', (0,0))
+rB2 = Bishop('', '', '', (0,0))
+rN2 = Knight('', '', '', (0,0))
+rR2 = Rook('', '', '', (0,0))
 
 Pieces = [gR1, gN1, gB1, gE, gK, gQ, gB2, gN2, gR2, gP1, gP2, gP3, gP4, gP5, gP6, gP7, gP8, gP9, rP1,rP2,rP3,rP4,rP5,rP6,rP7,rP8,rP9,rR1,rN1,rB1,rE,rK,rQ,rB2,rN2,rR2]
 def drawboard(screen):
@@ -124,13 +287,13 @@ i = []
 #putting pieces in board with for loop. '_' is an unoccupied space
 for i in range(0, 80):
   if i < 9:
-    Pieces[i] = Piece("g", pieces_names[i],('assets/Green_' + pieces_names[i]+ '.png'), ((64*i), 0))
+    Pieces[i] = Pieces[i].__class__("g", pieces_names[i],('assets/Green_' + pieces_names[i]+ '.png'), ((64*i), 0))
   elif 8 < i < 18:
    Pieces[i] = Pawn("g", 'Pawn',('assets/Green_Pawn.png'), ((64*(i-9), 64)))
   elif 17 < i  < 27:
     Pieces[i] = Pawn('r', 'Pawn', ('assets/Red_Pawn.png'), ((64*(i-18)), 64*7))
   elif 26 < i < 36:
-    Pieces[i] = Piece('r', pieces_names[(i-27)], ('assets/Red_' + pieces_names[i-27]+'.png'), ((64*(i-27), 64*8)))
+    Pieces[i] = Pieces[i].__class__('r', pieces_names[(i-27)], ('assets/Red_' + pieces_names[i-27]+'.png'), ((64*(i-27), 64*8)))
 
 
 
@@ -175,19 +338,13 @@ while running:
       
 
         for i in Pieces:
-          if isinstance(i, Pawn) or isinstance(i, Rook):
-            if (i.loc == (pos1[0], pos1[1])) and i.islegal(Pieces, pos1, pos2):
-              
+          if (i.loc == (pos1[0], pos1[1])):      
+            if i.islegal(Pieces, pos1, pos2):            
               pygame.draw.rect(screen, pygame.Color('blue'), pygame.Rect((pos1[0] - ((pos1[0]) % 64)), (pos1[1]-(pos1[1]%64)), 64, 64))
               i.loc = (pos2[0], pos2[1])
               pygame.draw.rect(screen, pygame.Color('purple'), pygame.Rect((pos2[0] - (pos2[0]) % 64), (pos2[1]-pos2[1]%64), 64, 64))
               drawpieces(Pieces)
-          elif isinstance(i, Piece):
-            if (i.loc == (pos1[0], pos1[1])):
-              pygame.draw.rect(screen, pygame.Color('blue'), pygame.Rect((pos1[0] - ((pos1[0]) % 64)), (pos1[1]-(pos1[1]%64)), 64, 64))
-              i.loc = (pos2[0], pos2[1])
-              pygame.draw.rect(screen, pygame.Color('purple'), pygame.Rect((pos2[0] - (pos2[0]) % 64), (pos2[1]-pos2[1]%64), 64, 64))
-              drawpieces(Pieces)
+
         
         pos1 = [-1,-1]
   
