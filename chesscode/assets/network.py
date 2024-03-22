6c57141c5aa61682c55a05225438a086ce8443df
BlueEgle <145373399+BlueEgle@users.noreply.github.com>
2024-03-22 07:35:33 -0400

    Update network.py

--- chesscode/assets/network.py
+++ chesscode/assets/network.py
@@ -2,7 +2,7 @@ import socket
 
 class Network:
     def __init__(self):
-        socket.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.server = '192.168.1.205'
         self.port = 5555
         self.addr = (self.server, self.port)
@@ -10,11 +10,17 @@ class Network:
         print(self.id)
     def connect(self):
         try: 
-            self.cliant.connect(self.addr)
+            self.client.connect(self.addr)
             return self.client.recv(2048).decode()
         except:
             pass
+    def send(self, data):
+        try:
+          self.client.send(str.encode(data))
+          return self.client.recv(2048).decode()
+        except socket.error as e:
+            print(e)
 
 n = Network()
-
-
+print(n.send('hello'))
+print(n.send('working'))
