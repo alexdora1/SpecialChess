9a63f356ae3944b0cd4a1cac37dd57d49c5cb4d1
BlueEgle <145373399+BlueEgle@users.noreply.github.com>
2024-03-23 17:09:36 -0400

    Added utf-8

--- chesscode/server.py
+++ chesscode/server.py
@@ -11,12 +11,12 @@ def createServer():
             print('Hola')
             (clientsocket, adress) = serversocket.accept()
             #running if someone has accepted
-            rd = clientsocket.recv(2048).decode()
+            rd = clientsocket.recv(2048).decode('utf-8')
             print(rd)
             #make sure we can send things
             #make sure we can recieve things
             data = 'hello world'
-            clientsocket.sendall(str(data).encode)
+            clientsocket.sendall(str(data).encode('utf-8'))
             clientsocket.shutdown(SHUT_WR)
             #splitting
     except KeyboardInterrupt:
