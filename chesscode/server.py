434ee5991cf335e4a67732e7ca038fe114f14c70
BlueEgle <145373399+BlueEgle@users.noreply.github.com>
2024-03-23 16:57:55 -0400

    Adjusting IP config

--- chesscode/server.py
+++ chesscode/server.py
@@ -4,7 +4,7 @@ def createServer():
     serversocket = socket(AF_INET, SOCK_STREAM)
     try:
         #ip adress of my mac, socket
-        serversocket.bind('192.168.1.205', 5555)
+        serversocket.bind(('localhost', 5555))
 
         serversocket.listen(2)
         while True:
@@ -12,6 +12,7 @@ def createServer():
             (clientsocket, adress) = serversocket.accept()
             #running if someone has accepted
             rd = clientsocket.recv(2048).decode()
+            print(rd)
             #make sure we can send things
             #make sure we can recieve things
             data = 'hello world'
