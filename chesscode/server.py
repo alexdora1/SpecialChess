4518d5839e4629ced1315895fd704506a2334f48
BlueEgle <145373399+BlueEgle@users.noreply.github.com>
2024-03-26 21:26:57 -0400

    Server sees all moves

--- chesscode/server.py
+++ chesscode/server.py
@@ -1,22 +1,55 @@
 from socket import *
+from threading import *
 
+Header = 64
+serversocket = socket(AF_INET, SOCK_STREAM)
+ADDR = ('192.168.1.230', 5555)
+serversocket.bind(ADDR)
+
+
+def client_interact(conn, addr):
+    print('NEW CONNECTION', addr, 'connected')
+    connected = True
+    while connected: 
+        msg = conn.recv(21).decode()
+        print(addr, " sent:", msg)
+        if msg == 'BYEBYEBYEBYEBYEBYEBYE':
+            connected =False
+    conn.close()
+
+
+
+def start():
+    serversocket.listen()
+    while True:
+        conn, addr = serversocket.accept()
+        thread = Thread(target = client_interact, args=(conn,addr))
+        thread.start()
+        print('ACTIVE CONNECTIONS:', (Thread.active_count() - 1))
+
+print('Server is starting')
+start()
+
+'''
 def createServer():
     serversocket = socket(AF_INET, SOCK_STREAM)
     try:
         #ip adress of my mac, socket
+
+
         serversocket.bind(('192.168.1.230', 5555))
 
         serversocket.listen(2)
         while True:
-            print('Hola')
+
             (clientsocket, adress) = serversocket.accept()
             #running if someone has accepted
-            rd = clientsocket.recv(2048).decode('utf-8')
+            rd = clientsocket.recv(2048).decode()
             print(rd)
             #make sure we can send things
             #make sure we can recieve things
             data = 'hello world'
-            clientsocket.sendall(str(data).encode('utf-8'))
+            clientsocket.sendall(str(data).encode())
             clientsocket.shutdown(SHUT_WR)
             #splitting
     except KeyboardInterrupt:
@@ -29,7 +62,7 @@ def createServer():
 print('Starting...')
 createServer()
 
-    
+''' 
 
 
 
